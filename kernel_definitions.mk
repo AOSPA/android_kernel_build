ba01206affd8dd1a0ebe41d932935df0d9c71e0c
Anay Wadhera <awadhera@berkeley.edu>
2020-10-24 12:04:30 -0700

    kernel_definitions: Account for all possible DTB paths.
    Change-Id: Idb915d2ca866c2445efeb90400f5d29593097004

--- kernel_definitions.mk
+++ kernel_definitions.mk
@@ -314,4 +314,4 @@ $(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
 
 # Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
 $(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
-	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@
+	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/**/*.dtb $(RTIC_DTB) > $@
